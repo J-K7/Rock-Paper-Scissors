001bfcba6125170287aaceb8517c42c23f8e7c9d
Jordie Keene <jordiekeene@yahoo.ca>
2022-05-01 18:11:04 -0400

    Made a loop to verify correct input from user and then created a function to play a single round of Rock, Paper, Scissors and declare a winner.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Function for the computer to randomly generate either rock, paper, or scissors
 function computerPlay() {
     let x = Math.floor(Math.random() * 3);
 
@@ -6,7 +7,35 @@ function computerPlay() {
     }else if (x === 1){
         x = 'Paper';
     } else if(x === 2){
-        x ='Scissors';
+        x = 'Scissors';
     }
-    console.log(x)
+    console.log(x);
+    return x;
+}
+
+// single round function parameters declared and playerSelection verified as an acceptable input
+const computerSelection = computerPlay();
+let playerSelection = prompt('Rock, Paper, or Scissors?').toLowerCase();
+
+while (playerSelection !== 'rock' && playerSelection !== 'paper' && playerSelection !== 'scissors') {
+    playerSelection = prompt('Rock, Paper, or Scissors?').toLowerCase();
+}
+// Function to play a single round of rock paper scissors and declare a winner
+function playRound(playerSelection, computerSelection) {
+
+    if (playerSelection === 'rock' && computerSelection ==='Paper') {
+        return 'You lose! Paper beats Rock.';
+    } else if (playerSelection === 'rock' && computerSelection === 'Scissors') {
+        return 'Congrats! You win.';
+    } else if (playerSelection === 'paper' && computerSelection === 'Scissors') {
+        return 'You lose! Scissors beats Paper.';
+    } else if (playerSelection === 'paper' && computerSelection === 'Rock') {
+        return 'Congrats! You win.';
+    } else if (playerSelection === 'scissors' && computerSelection === 'Rock') {
+        return 'You lose! Rock beats Scissors';
+    } else if (playerSelection === 'scissors' && computerSelection === 'Paper') {
+        return 'Congrats! You win.';
+    } else {
+        return 'Tie game. Play again';
+    }
 }
