b86910291038b24bb7a6d8df929f88fb730c1ca7
Jordie Keene <jordiekeene@yahoo.ca>
2022-05-23 20:51:17 -0400

    Made score function separate from game function. Added comments for clarity.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Function that generates a random number and then the corresponding rock, paper or scissors.
 function computerPlay() {
     let x = Math.floor(Math.random() * 3);
     
@@ -10,11 +11,15 @@ function computerPlay() {
     }
 }
 
+// Global variables
 let computerSelection = computerPlay();
 let playerSelection;
 let playerScore = 0;
 let computerScore = 0;
 let i;
+
+// Prompts the player to type their choice, either Rock, Paper, or Scissors and changes it to lower case.
+// Verifies correctness of their chocie and either returns it or prompts them again.
 
 function playerChoice() {
     playerSelection = prompt('Choose Rock, Paper, or Scissors').toLowerCase()
@@ -25,6 +30,8 @@ function playerChoice() {
         playerSelection = prompt('Choose Rock, Paper, or Scissors');
     }
 }
+
+// Plays a single round of rock, paper, scissors and console logs who the winner is. Incrememnts winner's score.
 function playRound(playerSelection, computerSelection) {
    
     if (playerSelection === 'rock' && computerSelection === 'paper' ||
@@ -51,10 +58,25 @@ function playRound(playerSelection, computerSelection) {
         }
 }
 
+// after rounds are completed this function determines who has the higher score and returns the winner and score.
+function score() {
+
+    if (playerScore > computerScore) {
+         return `Congrats you won the Game ${playerScore} - ${computerScore}`;
+   } else if (playerScore < computerScore) {
+       return `Too bad, The computer won ${computerScore} - ${playerScore}.`;
+   } else {
+       return `Tie game ${computerScore} - ${playerScore}.`;
+   }
+}
+
+// This function plays 5 rounds of Rock, Paper Scissors, and knokcs a point off if an incorrect input is given.
+// Declares winner usiing score function.
 function game() {
 
     for (let i = 0; i < 5; i++) {
-       
+        
+        
         let computerSelection = computerPlay();
         playerChoice();
 
@@ -63,11 +85,8 @@ function game() {
         }
     }
 
-    if (playerScore > computerScore) {
-        return `Congrats you won the Game ${playerScore} - ${computerScore}`;
-    } else if (playerScore < computerScore) {
-        return `Too bad, The computer won ${computerScore} - ${playerScore}.`;
-    } else {
-        return `Tie game ${computerScore} - ${playerScore}.`;
-    }
+    return score();
+
 }
+
+//When game() is called multiple times the score does not reset. Will need to be updated but too much for my tiny brain right now
